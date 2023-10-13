01e2fa0b943596d08bcfa1428ab378ab75ced868
barisdeniz <139981639+barisdeniz@users.noreply.github.com>
2023-10-13 12:26:57 +0300

    Letter_Pyramid

---
+++ Letter_Pyramid/main.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+size_t count{};
+string string1{};    
+size_t index1;
+int i;
+size_t n;
+int decrease=0;
+int k=0;
+int space=0;
+cout<<"Enter your string: ";
+getline(cin,string1);
+count=string1.size();
+int spaceCount=count;
+
+for (index1=1;index1<=count;index1++){
+    for(space=0;space<spaceCount;space++){
+      cout<<" ";  
+        
+        
+        
+    }
+    spaceCount=spaceCount-1;
+    for(n=0;n<index1;n++){
+    cout<<string1.at(n);
+    }
+    
+    
+    if(index1>1){
+    k++;    
+    for(i=0;i<k;i++){
+    cout<<string1.at(decrease-i);    
+    }   
+    decrease++;   
+    }
+    cout<<endl;
+    }
+}
